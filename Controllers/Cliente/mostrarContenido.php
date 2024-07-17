802a3ac3172048f1af80c01dc89a7b99066ad809
JuanDavidZabalaTapiero <juandavidzabalatapiero@gmail.com>
2024-07-16 20:00:20 -0500

    Añadí el btn de registro en el index y arregle el header

--- Controllers/Cliente/mostrarContenido.php
+++ Controllers/Cliente/mostrarContenido.php
@@ -4,8 +4,8 @@
 function mostrarHeader()
 {
 
-	echo '
-    <header>
+	?>
+	<header>
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
@@ -24,10 +24,10 @@ function mostrarHeader()
 								<li class="nav-item active">
 									<a class="nav-link" href="../../index.php">Inicio</a>
 								</li>
-								
+
 								<li class="nav-item dropdown dropdown-slide @@pages">
-									<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"
-										aria-haspopup="true" aria-expanded="false">
+									<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true"
+										aria-expanded="false">
 										Contenido <span><i class="fa fa-angle-down"></i></span>
 									</a>
 									<!-- Dropdown list -->
@@ -36,15 +36,18 @@ function mostrarHeader()
 
 										<li><a class="dropdown-item @@contact" href="contact-us.php">Contácto</a></li>
 
-										<li><a class="dropdown-item @@terms"
-												href="terms-condition.php">Términos y
+										<li><a class="dropdown-item @@terms" href="terms-condition.php">Términos y
 												Condiciones</a></li>
 									</ul>
 								</li>
-								
+
 								<li>
 									<a href="../Extras/iniciarSesion.php">
-										<svg xmlns="http://www.w3.org/2000/svg" height="32" width="36" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ca5d1e" d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
+										<svg xmlns="http://www.w3.org/2000/svg" height="32" width="36"
+											viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+											<path fill="#ca5d1e"
+												d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+										</svg>
 									</a>
 								</li>
 							</ul>
@@ -53,6 +56,10 @@ function mostrarHeader()
 								<li class="nav-item">
 									<a class="nav-link login-button" href="../Extras/iniciarSesion.php">Login</a>
 								</li>
+								<li class="nav-item">
+									<a class="nav-link text-white add-button"
+										href="../Extras/crearCuenta.html">Registrarse</a>
+								</li>
 							</ul>
 						</div>
 					</nav>
@@ -60,7 +67,7 @@ function mostrarHeader()
 			</div>
 		</div>
 	</header>
-    ';
+	<?php
 }
 
 function mostrarCategorias()
--- index.php
+++ index.php
@@ -12,6 +12,10 @@ require_once ('Models/consultasCliente.php');
 require_once (__DIR__ . '/Controllers/Cliente/contenidoCliente.php');
 $objContenidoCliente = new ContenidoCliente();
 
+// CONTENIDO PRINCIPAL
+require_once (__DIR__ . '/Controllers/contenidoMain.php');
+$objContenidoMain = new ContenidoMain();
+
 ?>
 <!DOCTYPE html>
 
@@ -60,70 +64,18 @@ $objContenidoCliente = new ContenidoCliente();
 <body class="body-wrapper">
 
 	<!-- HEADER -->
-	<header>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<nav class="navbar navbar-expand-lg navbar-light navigation">
-						<a class="navbar-brand" href="index.php">
-							<h2>Loviing Store</h2>
-						</a>
+	<?php
+	$urls = [
+		'index.php' => 'index.php',
+		'about-us.php' => 'Views/Website_externo/about-us.php',
+		'contact-us.php' => 'Views/Website_externo/contact-us.php',
+		'terms-condition.php' => 'Views/Website_externo/terms-condition.php',
+		'iniciarSesion.php' => 'Views/Extras/iniciarSesion.php',
+		'crearCuenta' => 'Views/Extras/crearCuenta.html'
+	];
 
-						<button class="navbar-toggler" type="button" data-toggle="collapse"
-							data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-							aria-expanded="false" aria-label="Toggle navigation">
-							<span class="navbar-toggler-icon"></span>
-						</button>
-						<div class="collapse navbar-collapse" id="navbarSupportedContent">
-							<ul class="navbar-nav ml-auto main-nav ">
-								<li class="nav-item active">
-									<a class="nav-link" href="index.php">Inicio</a>
-								</li>
-
-								<li class="nav-item dropdown dropdown-slide @@pages">
-									<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"
-										aria-haspopup="true" aria-expanded="false">
-										Contenido <span><i class="fa fa-angle-down"></i></span>
-									</a>
-									<!-- Dropdown list -->
-									<ul class="dropdown-menu">
-										<li><a class="dropdown-item @@about"
-												href="Views/Website_externo/about-us.php">Sobre nosotros</a></li>
-
-										<li><a class="dropdown-item @@contact"
-												href="Views/Website_externo/contact-us.php">Contácto</a></li>
-
-										<li><a class="dropdown-item @@terms"
-												href="Views/Website_externo/terms-condition.php">Términos y
-												Condiciones</a></li>
-									</ul>
-								</li>
-
-								<li>
-									<a href="Views/Extras/iniciarSesion.php">
-										<svg xmlns="http://www.w3.org/2000/svg" height="32" width="36"
-											viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-											<path fill="#ca5d1e"
-												d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
-										</svg>
-									</a>
-								</li>
-							</ul>
-
-							<ul class="navbar-nav ml-auto mt-10">
-								<li class="nav-item">
-									<a class="nav-link login-button" href="Views/Extras/iniciarSesion.php">Login</a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link text-white add-button" href="Views/Extras/crearCuenta.html">Registrarse</a>
-								</li>
-							</ul>
-						</div>
-					</nav>
-				</div>
-			</div>
-		</div>
-	</header>
+	$objContenidoMain->showHeader($urls);   
+	?>
 
 	<!--===============================
 	=            Hero Area            =
